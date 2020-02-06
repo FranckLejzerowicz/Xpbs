1bf09c71660aac889c63ede33a3c248bc983c60d
FranckLejzerowicz <franck.lejzerowicz@unige.ch>
2020-02-06 09:50:42 -0800

    add types

--- Xpbs/_check.py
+++ Xpbs/_check.py
@@ -9,7 +9,13 @@
 import sys
 
 
-def check_pbs(pbs):
+def check_pbs(pbs: list) -> None:
+    """
+    Ask user to check for the directives.
+
+    :param pbs: directives.
+    :return: None
+    """
     print('-------------')
     print('PBS commands:')
     print('-------------')
@@ -21,7 +27,14 @@ def check_pbs(pbs):
         sys.exit(1)
 
 
-def check_command(job_file, commands):
+def check_command(job_file: str, commands: list) -> None:
+    """
+    Ask user to check for the job content.
+
+    :param job_file: filename for the output script.
+    :param commands: commands to write.
+    :return: None
+    """
     print()
     print('Command lines to write in %s' % job_file)
     for command in commands:
