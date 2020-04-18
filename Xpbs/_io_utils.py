55befb0fd0cbcebee91168644983e78234dd33ce
FranckLejzerowicz <franck.lejzerowicz@unige.ch>
2020-04-18 14:19:30 -0700

    remove trailing slash

--- Xpbs/_io_utils.py
+++ Xpbs/_io_utils.py
@@ -83,14 +83,14 @@ def write_job(i_job: str, job_file: str, pbs: list, env: list, p_scratch_path: s
             for ff in sorted(ff_dirs):
                 if ff not in copied_dirs:
                     if ff[0] == '/':
-                        o.write('if [ -d ${locdir}%s/ ]; then rsync -auq ${locdir}%s %s; fi\n' % (ff, ff, ff))
+                        o.write('if [ -d ${locdir}%s ]; then rsync -auq ${locdir}%s %s; fi\n' % (ff, ff, ff))
                     else:
-                        o.write('if [ -d ${locdir}/%s/ ]; then rsync -auq ${locdir}/%s %s; fi\n' % (ff, ff, ff))
+                        o.write('if [ -d ${locdir}/%s ]; then rsync -auq ${locdir}/%s %s; fi\n' % (ff, ff, ff))
             for ff in set(outputs):
                 if ff[0] == '/':
-                    o.write('if [ -f ${locdir}%s/ ]; then rsync -auq ${locdir}%s %s; fi\n' % (ff, ff, ff))
+                    o.write('if [ -f ${locdir}%s ]; then rsync -auq ${locdir}%s %s; fi\n' % (ff, ff, ff))
                 else:
-                    o.write('if [ -f ${locdir}/%s/ ]; then rsync -auq ${locdir}/%s %s; fi\n' % (ff, ff, ff))
+                    o.write('if [ -f ${locdir}/%s ]; then rsync -auq ${locdir}/%s %s; fi\n' % (ff, ff, ff))
             if gpu:
                 o.write('cd $SLURM_SUBMIT_DIR\n')
             else:
