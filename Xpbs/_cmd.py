4ae3f9c95d518fe6528dd18d1a9e5d63ad1fc57d
FranckLejzerowicz <franck.lejzerowicz@unige.ch>
2020-05-05 13:54:19 -0700

    fix ffpaths

--- Xpbs/_cmd.py
+++ Xpbs/_cmd.py
@@ -34,7 +34,7 @@ def collect_ff(abs_line: str, ff_paths: set,
             ff_dirs.add(abs_i_s)
         elif isfile(abs_i_s):
             which_out = subprocess.getstatusoutput('which %s' % abs_i_s)
-            if not which_out[0]:
+            if which_out[0]:
                 ff_paths.add(abs_i_s)
         # get only local -> scratch folder paths mapping for root based folders
         elif len(abs_i_s) > 1 and abs_i_s[0] == '/':
